871f462265d5de815e95aca27b164297bcd000bd
HuangJianSheng0822 <3255186114@qq.com>
2023-11-02 13:59:24 +0800

    首次提交

---
+++ src/test/java/com/qn/qiniudemoapi/QiniudemoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qn.qiniudemoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QiniudemoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
